76304c262d377b0fe150ccbb7c8b8a510860fad1
glisco <glisco03@web.de>
2024-01-05 18:49:25 +0100

    1.20.4

--- src/main/java/com/glisco/disenchanter/VisitableTextContent.java
+++ src/main/java/com/glisco/disenchanter/VisitableTextContent.java
@@ -1,5 +1,6 @@
 package com.glisco.disenchanter;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.text.StringVisitable;
 import net.minecraft.text.Style;
 import net.minecraft.text.TextContent;
@@ -7,6 +8,9 @@ import net.minecraft.text.TextContent;
 import java.util.Optional;
 
 public record VisitableTextContent(StringVisitable content) implements TextContent {
+
+    private static final Type<VisitableTextContent> DUMMY_TYPE = new Type<>(MapCodec.unit(new VisitableTextContent(StringVisitable.EMPTY)), "disenchanter:visitable_text");
+
     @Override
     public <T> Optional<T> visit(StringVisitable.StyledVisitor<T> visitor, Style style) {
         return content.visit(visitor, style);
@@ -15,5 +19,10 @@ public record VisitableTextContent(StringVisitable content) implements TextConte
     @Override
     public <T> Optional<T> visit(StringVisitable.Visitor<T> visitor) {
         return content.visit(visitor);
+    }
+
+    @Override
+    public Type<?> getType() {
+        return DUMMY_TYPE;
     }
 }
