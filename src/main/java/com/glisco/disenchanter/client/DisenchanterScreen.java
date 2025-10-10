9175dc19f5046bc5357f6b35ce461894595f40de
Hunter <github@mail.redkey.sh>
2025-10-09 20:14:57 -0700

    fix: update GUI rendering for new RenderPipeline API
    DrawContext.drawTexture() now requires a RenderPipeline parameter in 1.21.9. Use RenderPipelines.GUI_TEXTURED for proper GUI texture rendering.
    
    Signed-off-by: Hunter <github@mail.redkey.sh>

--- src/main/java/com/glisco/disenchanter/client/DisenchanterScreen.java
+++ src/main/java/com/glisco/disenchanter/client/DisenchanterScreen.java
@@ -5,10 +5,11 @@ import com.glisco.disenchanter.DisenchanterNetworking;
 import com.glisco.disenchanter.DisenchanterScreenHandler;
 import com.glisco.disenchanter.catalyst.Catalyst;
 import com.glisco.disenchanter.catalyst.CatalystRegistry;
+
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -29,7 +30,7 @@ public class DisenchanterScreen extends HandledScreen<DisenchanterScreenHandler>
 
     @Override
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
-        context.drawTexture(RenderLayer::getGuiTextured, TEXTURE, this.x, this.y, 0, 0, this.backgroundWidth, this.backgroundHeight, 256, 256);
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE, this.x, this.y, 0, 0, this.backgroundWidth, this.backgroundHeight, 256, 256);
     }
 
     @Override
